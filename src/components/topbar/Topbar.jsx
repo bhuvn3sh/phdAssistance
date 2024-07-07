ebefb5b33730011302c754a23ffa2d076d3bbb35
Bhuvnesh <mail.bhuvnesh12@gmail.com>
2024-07-08 03:07:33 +0530

    updated topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -13,17 +13,18 @@ function topbar() {
             <div class="row">
                 <div class="col-md-6 d-flex align-items-center">
                     <p class="mb-0 phone pl-md-2">
-                        <a href="#" class="mr-2"><span class="fa fa-phone mr-1"></span> +00 1234 567</a> 
-                        <a href="#"><span class="fa fa-paper-plane mr-1 ml-4"></span> youremail@email.com</a>
+                        <a href="#" class="mr-2" style={{ textDecoration: 'none' }}><span class="fa fa-phone mr-1" ></span> +91-976966466</a> 
+                        <a href="#" style={{ textDecoration: 'none' }}><span class="fa fa-paper-plane mr-1 ml-4"></span> info@phdassistance.com</a>
                     </p>
                 </div>
                 <div class="col-md-6 d-flex justify-content-md-end">
                     <div class="social-media">
                     <p class="mb-0 d-flex">
-                        <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
-                        <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
-                        <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
-                        <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-dribbble"><i class="sr-only">Dribbble</i></span></a>
+                        <a href="https://www.facebook.com/phdassistance/" class="d-flex align-items-center justify-content-center" style={{ textDecoration: 'none' }}><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
+                        <a href="https://x.com/phdassistance/" class="d-flex align-items-center justify-content-center" style={{ textDecoration: 'none' }}><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
+                        <a href="https://www.instagram.com/phdassistance/" class="d-flex align-items-center justify-content-center" style={{ textDecoration: 'none' }}><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
+                        <a href="https://www.youtube.com/channel/UC-mKbdBTZNv5bBoXsMqxqkQ" class="d-flex align-items-center justify-content-center" style={{ textDecoration: 'none' }}><span class="fa fa-youtube"><i class="sr-only">Youtube</i></span></a>
+                        <a href="https://www.linkedin.com/company/phd-assistance/" class="d-flex align-items-center justify-content-center" style={{ textDecoration: 'none' }}><span class="fa fa-linkedin"><i class="sr-only">LinkedIn</i></span></a>
                     </p>
             </div>
                 </div>
